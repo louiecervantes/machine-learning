53257f6d7fc25c37daf3e4080598e187b8b6a71c
Louie Cervantes <128763658+louiecervantes@users.noreply.github.com>
2023-03-29 01:49:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,14 @@ def app():
     # Load the mobile phone dataset
     df=pd.read_csv('mobilephones.csv', header=0)
     st.dataframe(df, width=800, height=400)
-    
+    desc = data.describe().T
+    df1 = pd.DataFrame(index=['battery_power', 'blue', 'clock_speed', 'dual_sim',
+                          'fc', 'four_g', 'int_memory', 'm_dep', 'mobile_wt', 
+                          'n_cores', 'pc', 'px_height', 'px_width', 'ram', 
+                          'sc_h', 'sc_w', 'talk_time', 'three_g','touch_screen',
+                          'wifi', 'price_range'], 
+                   columns= ["count","mean","std","min",
+                             "25%","50%","75%","max"], data= desc )
     if st.button('Display Info'):
         fig, ax = plt.subplots(figsize=(12,12))
         sns.heatmap(df1, annot=True,cmap = "Blues", fmt= '.0f',
