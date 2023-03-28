10fcac2af40eed76256edb9e9132a085418d8d18
Louie Cervantes <128763658+louiecervantes@users.noreply.github.com>
2023-03-29 01:46:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,12 +21,22 @@ def app():
     st.subheader("Louie F. Cervantes M.Eng. \n(c) 2023 WVSU College of ICT")
     
     st.title("This app will compare machine learning algorithms")
-    st.write('We use a dataset of mobile phone attributes to train the ML classifiers. We then split the data into training and test sets. Each classifier is tasked to predict the pricing class then the accuracy of each one is measured by counting the number of correct predictions over the entire test set')
+    st.write('We use a dataset of mobile phone attributes to train the ML classifiers. We then split the data into training and test sets. Each classifier is tasked to predict the pricing class then the accuracy of each one is measured by counting the number of correct predictions over the entire test set.')
 
     # Load the mobile phone dataset
     df=pd.read_csv('mobilephones.csv', header=0)
     st.dataframe(df, width=800, height=400)
-
+    
+    if st.button('Display Info'):
+        fig, ax = plt.subplots(figsize=(12,12))
+        sns.heatmap(df1, annot=True,cmap = "Blues", fmt= '.0f',
+        ax=ax,linewidths = 5, cbar = False,
+        annot_kws={"size": 16})
+        plt.xticks(size = 18)
+        plt.yticks(size = 12, rotation = 0)
+        plt.ylabel("Variables")
+        plt.title("Descriptive Statistics", size = 16)
+        st.pyplot(fig)
      
 
 # Run the app
